6d057e88f389cb323f3492d5cb35fd0ddd5d9634
AlfonsoGonzalezOrtiz <80314412+AlfonsoGonzalezOrtiz@users.noreply.github.com>
2022-01-13 19:11:23 +0100

    Modelo

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from Modelo.tClient import tClient
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    c = tClient("a","a","a",1)
+    print(c.__str__())
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
